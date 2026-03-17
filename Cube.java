5bc62dcffa37f58817185944c72e4c21b9916c1c
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-03-17 13:30:46 -0500

    fixed improperly connected edges

--- Cube.java
+++ Cube.java
@@ -2,26 +2,22 @@ public class Cube {
 
     public Vector[] vertices = {
         //Z=1
-        new Vector(-1, 1, 1),
-        new Vector(1, 1, 1),
-        new Vector(-1, -1, 1),
-        new Vector(1, -1, 1),
+        new Vector(-1, 1, 1),     //0
+        new Vector(1, 1, 1),    //1
+        new Vector(-1, -1, 1),       //2
+        new Vector(1, -1, 1),     //3
 
         //Z=-1
-        new Vector(-1, 1, -1),
-        new Vector(1, 1, -1),
-        new Vector(1, -1, -1),
-        new Vector(-1, -1, -1)
+        new Vector(-1, 1, -1),      //4
+        new Vector(1, 1, -1),     //5
+        new Vector(1, -1, -1),      //6
+        new Vector(-1, -1, -1)         //7
     };
 
     //undirected graph
     public int[][] edges = {
-        {0,1}, {0,2}, {0,4}, //node0 connected vertices 1,2,4
-        {1,3}, {1, 5},
-        {2, 3}, {2, 6},
-        {3,7},
-        {4,5}, {4,6},
-        {5,6},
-        {6,7}
-    };
+        {0,1},{1,3},{3,2},{2,0},    //front face edges
+        {4,5},{5,6},{6,7},{7,4},    //back
+        {0,4},{1,5},{2,7},{3,6}     //sides
+    }; 
 }
